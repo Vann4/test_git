0eb36bd3ab3ddb92a2c6526ab08085b6f6bdf79c
Vann4 <1vetlan1@mail.ru>
2023-09-03 17:37:00 +0300

    Edit .gitignore and edit main.py add if

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import math
 print('')
 print('Найдем дискриминант\n')
-input('Введите число a: ')
-input('Введите число b: ')
-input('Введите число c: ')
+a = int(input('Введите число a: '))
+b = int(input('Введите число b: '))
+c = int(input('Введите число c: '))
+
+D = b**2 - 4*a*c
+
+if D > 0:
+    x1 = ((-b+math.sqrt(D)) / (2*a))
+    x2 = ((-b-math.sqrt(D)) / (2*a))
